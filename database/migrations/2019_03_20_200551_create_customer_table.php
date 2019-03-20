5712733a018ac22e1faf8a04bf30ffe37016e131
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-03-20 22:13:52 +0200

    add customer table and reference it with users table

---
+++ database/migrations/2019_03_20_200551_create_customer_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->date('date_of_birth');
+            $table->enum('gender',['M','F']);
+            $table->integer('remaining_sessions');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}

9a15c4d35cc4cf3b62e33fdac504b482896a55e3
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-03-21 00:54:59 +0200

    gym table migrations

---
+++ database/migrations/2019_03_20_224942_create_gyms_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGymsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gyms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('created_at');
+            $table->string('img');
+            $table->string('creator_name');
+            $table->bigInteger('city_id')->unsigned();
+            $table->foreign('city_id')
+                ->references('id')
+                ->on('cities')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gyms');
+    }
+}

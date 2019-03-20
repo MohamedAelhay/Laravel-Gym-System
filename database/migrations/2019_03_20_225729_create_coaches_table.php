0dd77c935a5cfd8725852ec427029392428a6f5e
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-03-21 01:26:25 +0200

    sessions&coaches migrations

---
+++ database/migrations/2019_03_20_225729_create_coaches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoachesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coaches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('gym_id')->unsigned();
+            $table->foreign('gym_id')
+                ->references('id')
+                ->on('gyms')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coaches');
+    }
+}

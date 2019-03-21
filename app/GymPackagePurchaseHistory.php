126e421606e474e77afefee4e70a23e9aa1d624f
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-03-21 13:54:02 +0200

    Add Fillable fields and Table name

--- app/GymPackagePurchaseHistory.php
+++ app/GymPackagePurchaseHistory.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class GymPackagePurchaseHistory extends Model
 {
-    //
+    protected $fillable = ['package_name', 'package_price','purchase_date','user_id', 'gym_id'];
+    protected $table = 'gym_packages_purchase_history';
 }

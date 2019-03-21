d317e7df47cfd0f781f08b41b9b70b5c2ae1125f
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-03-21 22:46:50 +0200

    add relations to City, CityManager,Country,Gym,GymPackage,User : Models

---
+++ app/Role.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public function user(){
+
+        return $this->belongsToMany(User::class);
+    }
+
+    protected $table = 'roles';
+
+    protected $timestamps = false;
+}

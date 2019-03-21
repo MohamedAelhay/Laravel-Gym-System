1182f3374d3471ce5a4c6b8c5609d59e88f256cf
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-03-21 02:14:24 +0200

    countries package & add country fk in cities table

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        'Webpatser\Countries\CountriesServiceProvider',
+
     ],
 
     /*
@@ -225,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+        'Countries' => 'Webpatser\Countries\CountriesFacade',
 
     ],
 

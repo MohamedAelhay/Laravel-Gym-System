5936bb426abb721ac0d4d88d6a9e9882858c860e
Asmaa Saeed <asmaasaeed057@gmail.com>
2019-03-21 23:40:55 +0200

    add Admin LTE

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', function () {
+    return view('admin');
+});

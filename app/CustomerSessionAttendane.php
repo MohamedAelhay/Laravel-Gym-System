676b1d6980e2911e1ec8a82cf332d2f1547775da
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-03-21 13:51:58 +0200

    Add Fillable fields and Table name

--- app/CustomerSessionAttendane.php
+++ app/CustomerSessionAttendane.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CustomerSessionAttendane extends Model
 {
-
+    protected $fillable = ['attendance_time', 'attendance_date', 'user_id', 'session_id'];
+    protected $table = 'customer_sessions_attendance';
 }

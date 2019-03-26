6ac2812ad8ba82de0040cbdb3501a37bdf6ac039
Ahmed Ibrahim <ah_ibrahim94@hotmail.com>
2019-03-26 02:14:06 +0200

    update gym manager request validationn

--- app/Http/Requests/GymManager/UpdateGymManagerRequest.php
+++ app/Http/Requests/GymManager/UpdateGymManagerRequest.php
@@ -13,7 +13,7 @@ class UpdateGymManagerRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,28 @@ class UpdateGymManagerRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'=>"required|Alpha|max:25|min:6",
+            'email' => "required|email",
+            'gym_id'=>"exists:gyms,id",
+            'img' => 'mimes:jpeg,jpg,png | max:2000',
+            'national_id' => 'required|max:25|min:6',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+
+            'name.required' => 'This field Can\'t be Empty !',
+            'name.max' => 'Name must be less than 25 Character !',
+            'name.min' => 'Name must be more than 6 Character !',
+            'gym_id.exists' => 'Gym Must Be Exist !',
+            'img.mimes' => 'Image Format must be (jpeg,jpg,png)',
+            'img.max' => 'Image Max Size is 2000kb',
+            'email.required' => 'Email is Required',
+            'email.email' => 'Not valid Email Format',
+            'national_id.required' => 'This field Can\'t be Empty !',
+
         ];
     }
 }
--- resources/views/GymManagers/edit.blade.php
+++ resources/views/GymManagers/edit.blade.php
@@ -9,28 +9,62 @@
         <div class="form-group row">
             <label for="name" class="col-sm-2 col-form-label" >Name</label>
             <input type="text" name="name" class="form-control col-3" value="{{$gymManager->name}}">
+            @if ($errors->get('name'))
+                <div class="alert alert-danger col-sm-6" style="margin-left: 20px; margin-top: 10px;" >
+                    <ul>
+                        @foreach ($errors->get('name') as $name)
+                            <li>{{ $name }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
         <div class="form-group row">
             <label for="email" class="col-sm-2 col-form-label" >Email</label>
             <input type="email" name="email"  class="form-control col-3" value="{{$gymManager->email}}">
+            @if ($errors->get('email'))
+                <div class="alert alert-danger col-sm-6" style="margin-left: 20px; margin-top: 10px;" >
+                    <ul>
+                        @foreach ($errors->get('email') as $email)
+                            <li>{{ $email }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
-        <div class="form-group row">
-            <label for="password" class="col-sm-2 col-form-label" >Password</label>
-            <input type="password" name="password"  class="form-control col-3">
         </div>
         <div class="form-group row">
             <label for="national_id" class="col-sm-2 col-form-label" >National ID</label>
             <input type="text" name="national_id"  class="form-control col-3" value="{{$gymManager->role->national_id}}">
-            <input type="hidden" name="role_type"  class="form-control col-3" value="App\GymManager">
+            @if ($errors->get('national_id'))
+                <div class="alert alert-danger col-sm-6" style="margin-left: 20px; margin-top: 10px;" >
+                    <ul>
+                        @foreach ($errors->get('national_id') as $national_id)
+                            <li>{{ $national_id }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
         <div class="form-group row">
             <label for="img" class="col-sm-2 col-form-label" >Img</label>
             <div class="col">
                 @if(Storage::disk('public')->has($gymManager->name.'-'.$authUser->name.'.jpg'))
-                    <img src="{{asset('storage/'.$gymManager->name.'-'.$authUser->name.'.jpg')}}" style="width: 200px; height: 200px;" class="img-fluid" alt="Responsive image">
+                    <img src="{{asset('storage/'.$gymManager->name.'-'.$authUser->name.'.jpg')}}"
+                         style="width: 200px; height: 200px;" class="img-fluid" alt="Responsive image">
                 @endif
             </div>
             <input type="file" name="img" class="form-control col-3">
+            @if ($errors->get('img'))
+                <div class="alert alert-danger col-sm-6" style="margin-left: 20px; margin-top: 10px;" >
+                    <ul>
+                        @foreach ($errors->get('img') as $img)
+                            <li>{{ $img }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
         </div>
         <div class="form-group row">
             <label for="gym_id" class="col-sm-2 col-form-label">Gyms</label>
@@ -39,6 +73,15 @@
                     <option value="{{$gym->id}}" >{{$gym->name}}</option>
                 @endforeach
             </select>
+            @if ($errors->get('gym_id'))
+                <div class="alert alert-danger col-sm-6" style="margin-left: 20px; margin-top: 10px;" >
+                    <ul>
+                        @foreach ($errors->get('gym_id') as $gym_id)
+                            <li>{{ $gym_id }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>

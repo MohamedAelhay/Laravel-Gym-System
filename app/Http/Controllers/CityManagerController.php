7c4cca56b4ef1c77efad55e07c0c67eb1f83db61
Asmaa Saeed <asmaasaeed057@gmail.com>
2019-03-27 18:33:41 +0200

    modify in city manager->show->create

--- app/Http/Controllers/CityManagerController.php
+++ app/Http/Controllers/CityManagerController.php
@@ -45,18 +45,30 @@ class CityManagerController extends Controller
         ]);
     }
 
+
+    public function storeImage($request,$user){
+
+        $file = $request->file('img');
+        $fileName = $request['name'].'-'.$user->name.'.jpg';
+        if ($file){
+            Storage::disk('public')->put($fileName,File::get($file));
+        }
+        return  $fileName;
+    }
   
     public function store(StoreCityManagerRequest $request)
     {
 //        dd($request);
-        //here
+        $user = auth()->user(); 
         $city_manger = CityManager::create($request->all());
+        $request['img'] = $this->storeImage($request,$user);
         User::create([
             'name'=>$request->name,
             'email'=>$request->email,
             'password'=>bcrypt($request['password']),
             'role_id'=>$city_manger->id,
             'role_type'=>get_class($city_manger),
+            'img'=> $request['img'],
         ])->assignRole('city-manager');
         
         return redirect()->route('CityManagers.index');
@@ -66,11 +78,11 @@ class CityManagerController extends Controller
 
     public function show($cityManagerId)
     {
-    
+        $authUser = auth()->user();
         $cityManager = User::findOrFail($cityManagerId);
         return view('CityManagers.show', [
             'cityManager' => $cityManager,
-           
+            'authUser' => $authUser
         ]);
     }
 

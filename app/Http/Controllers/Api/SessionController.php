9f12fe1c6fe3b6fa4907ff5f952c26b111403785
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-03-28 23:02:51 +0200

    User Attendance History

--- app/Http/Controllers/Api/SessionController.php
+++ app/Http/Controllers/Api/SessionController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Customer;
+use App\Gym;
+use App\Session;
 use App\CustomerSessionAttendane;
 use App\GymPackage;
 use App\GymPackagePurchaseHistory;
@@ -38,5 +39,26 @@ class SessionController extends Controller
         ], 200);
     }
 
+    public function userSessionsHistory()
+    {
+        $user = Auth::user();
+        $attendance= CustomerSessionAttendane::where('user_id', $user->id)->get();
 
+        $respone = [];
+        foreach ($attendance as $attend)
+        {
+            $session = Session::where('id', $attend->session_id)->first();
+            $gym = Gym::where('id', $session->gym_id)->first();
+            $respone[] = [
+                'Session Name' => $session->name,
+                'Gym Name' => $gym->name,
+                'Attendance Date' => $attend->attendance_date,
+                'Attendance Time' => $attend->attendance_time
+            ];
+        }
+
+        return response()->json([
+            'Session History' => $respone,
+        ], 200);
+    }
 }

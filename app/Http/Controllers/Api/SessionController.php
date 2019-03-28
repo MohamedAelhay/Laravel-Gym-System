d883f7039dfe0a6b33ddc961641afc278860a92a
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-03-28 22:44:31 +0200

    Total and Remaining Sessions

--- app/Http/Controllers/Api/SessionController.php
+++ app/Http/Controllers/Api/SessionController.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Customer;
+use App\CustomerSessionAttendane;
+use App\GymPackage;
+use App\GymPackagePurchaseHistory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
-    //
+    public function userSessions()
+    {
+        $user = Auth::user();
+        $purchases = GymPackagePurchaseHistory::where('user_id', $user->id)->get();
+        $attendance= CustomerSessionAttendane::where('user_id', $user->id)->get();
+
+        $totalSession = 0;
+        foreach ($purchases as $purchase)
+        {
+            $package = GymPackage::where('id', $purchase->package_id)->first();
+
+            $totalSession += $package->number_of_sessions;
+        }
+
+        $remainingSession = $totalSession;
+        foreach ($attendance as $attend)
+        {
+            $remainingSession --;
+        }
+
+        return response()->json([
+            'Total Session' => $totalSession,
+            'Remaining Session' => $remainingSession,
+        ], 200);
+    }
+
+
 }

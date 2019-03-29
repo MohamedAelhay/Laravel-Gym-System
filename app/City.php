480da3c45ceb6feb20eb1c2f7f7eb4b5ba2e712f
Asmaa Saeed <asmaasaeed057@gmail.com>
2019-03-29 13:30:17 +0200

    modify in update validation

--- app/City.php
+++ app/City.php
@@ -12,16 +12,9 @@ class City extends Model
 
     public $timestamps = false;
 
-<<<<<<< HEAD
-    public function cityManager(){
-
-        // return $this->hasOne(CityManager::class);
-        return $this->belongsTo('App\CityManager','city_manager_id');
-=======
     public function cityManager()
     {
         return $this->belongsTo(CityManager::class, 'city_manager_id');
->>>>>>> 6e2060c910857d34c929d739df1b6b1f5ba0d367
     }
 
     public function gyms()
--- resources/views/Cities/create.blade.php
+++ resources/views/Cities/create.blade.php
@@ -1,3 +1,5 @@
+
+
 @extends('admin')
 
 
@@ -5,6 +7,15 @@
 <a href="{{route('Cities.index')}}" class="btn btn-danger">Back</a>
     <form action="{{route('Cities.store')}}" method="POST" style="margin-left: 20px">
         @csrf
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
         <div class="form-group row">
                 <label for="city name" class="col-sm-2 col-form-label" >city name</label>
                 <input type="text" name="city name" class="form-control col-3" placeholder="city name">

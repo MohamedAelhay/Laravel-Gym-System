e5f6438f94db7185e58a3d143d65af4ffcb42a00
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-03-29 09:52:29 +0200

    modyfing city manager role

--- app/City.php
+++ app/City.php
@@ -6,28 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    protected $fillable = ['id','name', 'city_manager_id', 'country_id'];
+    protected $fillable = ['id', 'name', 'city_manager_id', 'country_id'];
 
     protected $table = 'cities';
 
     public $timestamps = false;
 
-    public function cityManager(){
-
-        return $this->hasOne(CityManager::class);
-
+    public function cityManager()
+    {
+        return $this->belongsTo(CityManager::class, 'city_manager_id');
     }
 
-    public function gyms(){
-
+    public function gyms()
+    {
         return $this->hasMany(Gym::class);
-
     }
 
-    public function country(){
-
+    public function country()
+    {
         return $this->belongsTo(Country::class);
-
     }
-
 }

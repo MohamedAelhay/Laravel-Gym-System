8a8d97f6146643adc851b2bc9a20a2bb47dbfa0c
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-03-30 21:06:28 +0200

    adjusting migration for api

--- database/migrations/2019_03_28_185432_add_package_id_column_to_gym_package_purchase_history_table.php
+++ database/migrations/2019_03_28_185432_add_package_id_column_to_gym_package_purchase_history_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddPackageIdColumnToGymPackagePurchaseHistoryTable extends Migration
 {
@@ -14,7 +14,7 @@ class AddPackageIdColumnToGymPackagePurchaseHistoryTable extends Migration
     public function up()
     {
         Schema::table('gym_packages_purchase_history', function (Blueprint $table) {
-            $table->bigInteger('package_id')->unsigned();
+            $table->bigInteger('package_id')->unsigned()->nullable();
             $table->foreign('package_id')
                 ->references('id')
                 ->on('gym_packages')

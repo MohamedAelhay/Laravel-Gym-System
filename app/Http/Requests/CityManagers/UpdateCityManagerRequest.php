57d8bf3efe95fc1022ae2a6349ee5400faeabfd3
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-03-30 21:11:58 +0200

    modify update city validation

--- app/Http/Requests/CityManagers/UpdateCityManagerRequest.php
+++ app/Http/Requests/CityManagers/UpdateCityManagerRequest.php
@@ -24,40 +24,35 @@ class UpdateCityManagerRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>"required|Alpha|min:6|max:25",
-            'email'=>'required|email',
-            'password'=>"required|min:6",
-            'national_id'=>'required|numeric|min:5|unique:CityManagers,national_id,'.$this->CityManager['id'],
+            'name' => "Alpha|min:6|max:25",
+            'email' => 'email',
+            'password' => "min:6",
+            // 'national_id' => 'numerm,ic|min:5|unique:city_managers',
             // 'img' => 'mimes:jpeg,jpg,png | max:2000'
         ];
     }
 
-
-
-
-    public function messages() 
-
+    public function messages()
     {
-        return[
-        'name.required' => 'Name is required',
-        'name.min' => 'Name must be more than 6 Character !',  
-        'name.max' => 'Name must be less than 25 character',
-        
-        'email.required' => 'you must enter your email',
-        'email.email' => 'please enter a valid email',  
+        return [
+            'name.required' => 'Name is required',
+            'name.min' => 'Name must be more than 6 Character !',
+            'name.max' => 'Name must be less than 25 character',
 
+            'email.required' => 'you must enter your email',
+            'email.email' => 'please enter a valid email',
 
-        'password' =>'password is required',
-        'password.min'=>'password must be at least 6 characters',
+            'password' => 'password is required',
+            'password.min' => 'password must be at least 6 characters',
 
-        'national_id.required' => 'you must enter your national id',
-        'national_id.digits' => 'must be numbers',
-        'national_id.min' =>'national id must be at least 5',
-       
-        // 'password' =>'password is required',
-        // 'password.min'=>'password must be at least 6 characters',
-        // 'img.max' => 'image Max Size is 2000kb',
-        // 'img.mimes' => 'image extensins must be jpeg or jpg or png'
+            'national_id.required' => 'you must enter your national id',
+            'national_id.digits' => 'must be numbers',
+            'national_id.min' => 'national id must be at least 5',
+
+            // 'password' =>'password is required',
+            // 'password.min'=>'password must be at least 6 characters',
+            // 'img.max' => 'image Max Size is 2000kb',
+            // 'img.mimes' => 'image extensins must be jpeg or jpg or png'
         ];
     }
 }

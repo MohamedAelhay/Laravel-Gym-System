437687bf1476645e47e8377babe46f454be4d43f
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-03-30 21:09:21 +0200

    modify city manager update

--- app/Http/Controllers/CityManagerController.php
+++ app/Http/Controllers/CityManagerController.php
@@ -2,111 +2,134 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\City;
+use App\CityManager;
 use App\Http\Requests\CityManagers\StoreCityManagerRequest;
 use App\Http\Requests\CityManagers\UpdateCityManagerRequest;
-use App\CityManager;
-use App\City;
 use App\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class CityManagerController extends Controller
 {
-
-   public function index()
+    public function index()
     {
         $cityManagers = CityManager::all();
-        return view('CityManagers.index',[
-       
+        return view('CityManagers.index', [
+
             'cityManagers' => $cityManagers,
         ]);
     }
 
-  
     public function create()
     {
-        $cities=City::all();
+        $cities = City::all();
         $cityManagers = User::all();
-        return view('CityManagers.create',[
+        return view('CityManagers.create', [
             'cityManager' => $cityManagers,
-            'cities' => $cities
+            'cities' => $cities,
         ]);
     }
 
-
-    public function storeImage($request,$user){
-
-        $file = $request->file('img');
-        $fileName = $request['name'].'-'.$user->name.'.jpg';
-        if ($file){
-            Storage::disk('public')->put($fileName,File::get($file));
-        }
-        return  $fileName;
-    }
-  
     public function store(StoreCityManagerRequest $request)
     {
-        $user = auth()->user(); 
-        $city_manger = CityManager::create($request->all());
-        $path = Storage::putFile('public/managers', $request->file('img'));
-        User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request['password']),
-            'role_id'=>$city_manger->id,
-            'role_type'=>get_class($city_manger),
-            'img'=> $path,
-        ])->assignRole('city-manager');
-        
-        return redirect()->route('CityManagers.index');
+        $user = auth()->user();
+        $request = $this->hashPassword($request);
+        $request['img'] = $this->storeImage($request, $user);
+        $this->storeCityManagerData($request);
+        return redirect()->route('CityManagers.index')->with('success', 'City Manager created successfully!');
     }
 
+    public function storeImage($request, $user)
+    {
+        $file = $request->file('img');
+        $fileName = $request['name'] . '-' . $user->name . '.jpg';
+        if ($file) {
+            Storage::disk('public')->put($fileName, File::get($file));
+        }
+        return $fileName;
+    }
 
+    public function hashPassword($request)
+    {
+        $request['password'] = bcrypt($request['password']);
+        return $request;
+    }
+
+    public function storeCityManagerData($request)
+    {
+        $cityManager = CityManager::create($request->only(['national_id']));
+        $request->request->add(['role_id' => $cityManager->id, 'role_type' => get_class($cityManager)]);
+        User::create($request->only([
+            'name', 'email', 'password', 'img', 'role_id', 'role_type']))
+            ->assignRole('city-manager');
+    }
 
     public function show($cityManagerId)
     {
         $authUser = auth()->user();
-        $cityManager = User::findOrFail($cityManagerId)->first();
-        // dd($cityManager->img);
+        $cityManager = User::findOrFail($cityManagerId);
         return view('CityManagers.show', [
             'cityManager' => $cityManager,
-            'authUser' => $authUser
+            'authUser' => $authUser,
         ]);
     }
 
     public function edit($cityManagerId)
     {
+        $authUser = auth()->user();
         $cityManager = User::findOrFail($cityManagerId);
         return view('CityManagers.edit', [
             'cityManager' => $cityManager,
+            'authUser' => $authUser,
+
         ]);
     }
 
-    public function update(UpdateCityManagerRequest $request,$cityManagerId)
+    public function update(UpdateCityManagerRequest $request, $cityManagerId)
     {
+        // $cityManager = User::findOrFail($cityManagerId);
+        // $cityManager->update($request->all());
+        // $cityManager->role->update($request->all());
+        // return redirect()->route('CityManagers.index');
 
+        $user = auth()->user();
         $cityManager = User::findOrFail($cityManagerId);
+        $request['password'] = $this->updatePassword($cityManager->password, $request);
         $cityManager->update($request->all());
-        $cityManager->role->update($request->all());
-        return redirect()->route('CityManagers.index');
+        CityManager::findOrFail($cityManager->role->id)->update($request->all());
+        return redirect()->route('CityManagers.index')->with('success', 'City Manager edited successfully!');
     }
 
-   
+    public function updatePassword($oldPassword, $newPasswordRequest)
+    {
+        if (!$this->isChanged($oldPassword, $newPasswordRequest['password'])
+            || $newPasswordRequest['password'] == '******') {
+            return $oldPassword;
+        }
+
+        return $this->hashPassword($newPasswordRequest)['password'];
+    }
+
+    public function isChanged($oldValue, $newValue)
+    {
+        if ($oldValue == $newValue) {
+            return false;
+        }
+        return true;
+    }
+
     public function destroy($cityManagerId)
     {
         $user = User::findOrFail($cityManagerId);
         CityManager::findOrFail($user->role->id)->delete();
         $user->removeRole('city-manager');
         $user->delete();
-        
     }
 
     public function getCityManagers()
-
     {
-        return datatables()->of(CityManager::with('user'))->toJson();
+        return datatables()->of(CityManager::with(['user', 'city']))->toJson();
     }
-   
 }
--- resources/views/CityManagers/edit.blade.php
+++ resources/views/CityManagers/edit.blade.php
@@ -19,35 +19,22 @@
     @method('put')
 
     <div class="form-group">
-        <label for="national_id">National_ID</label>
-        <input name="national_id" value="{{$cityManager->role->national_id}}" type="text" class="form-control" id="national_id" aria-describedby="emailHelp" placeholder="Enter Title">
-    </div>
-    <div class="form-group">
         <label for="name">name</label>
         <input name="name" class="form-control" value="{{$cityManager->name}}">
     </div>
-    
+
     <div class="form-group">
          <label for="email">email</label>
          <input name="email" class="form-control" value="{{$cityManager->email}}">
      </div>
 
-      
+
     <div class="form-group">
      <label for="password">password</label>
-     <input name="password" type="password" class="form-control" value="{{$cityManager->password}}">
+     <input name="password" type="password" class="form-control" value="******">
  </div>
 
- <div class="form-group">
-     <label for="exampleInputPassword1">image</label>
-     <img src="{{$cityManager->image}}">
- </div>
 <button type="submit" class="btn btn-primary">Update</button>
 </form>
-    
+
 @endsection
-
-
-
-
-
